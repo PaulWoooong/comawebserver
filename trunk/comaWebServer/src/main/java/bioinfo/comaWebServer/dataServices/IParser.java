76e833db5ef3511e6a0cc70e8f082866218ff81b
Mindaugas.Laganeckas <Mindaugas.Laganeckas@549d8ce6-d049-11de-83be-cfe0592a616e>
2010-02-09 08:01:56 +0000

    Wrote a function for counting sequences in the multiple alignment.

--- trunk/comaWebServer/src/main/java/bioinfo/comaWebServer/dataServices/IParser.java
+++ trunk/comaWebServer/src/main/java/bioinfo/comaWebServer/dataServices/IParser.java
@@ -8,6 +8,7 @@ import bioinfo.comaWebServer.entities.ComaResults;
 
 public interface IParser 
 {
-	public abstract void parse(ComaResults comaResults, String path) throws IOException;
-	public abstract Map<String, AlignmentBibliography> parseIDS(String path) throws IOException;
+	public void parse(ComaResults comaResults, String path) throws IOException;
+	public Map<String, AlignmentBibliography> parseIDS(String path) throws IOException;
+	public int numberOfSeq(String path) throws IOException;
 }
