2e8e5eab45c6048877963dcf6419a8425cec7639
Mindaugas.Laganeckas <Mindaugas.Laganeckas@549d8ce6-d049-11de-83be-cfe0592a616e>
2010-03-01 06:52:44 +0000

    Forgot to include PSI BLAST filters into option group supervised by java script.

--- trunk/comaWebServer/src/main/resources/bioinfo/comaWebServer/components/ViewSearch.js
+++ trunk/comaWebServer/src/main/resources/bioinfo/comaWebServer/components/ViewSearch.js
@@ -2,11 +2,10 @@ function disableGroup()
 {
     document.getElementById('seqDB').disabled=true;
     document.getElementById('lc_t').disabled=true;
-    
     document.getElementById('aa').disabled=true;
-    
     document.getElementById('lc_h').disabled=true;
     document.getElementById('lc_j').disabled=true;
+    document.getElementById('psi_blast_filters').disabled=true;
 }
 
 function enableGroup()
@@ -16,4 +15,5 @@ function enableGroup()
     document.getElementById('lc_h').disabled=false;
     document.getElementById('lc_j').disabled=false;
     document.getElementById('lc_t').disabled=false;
+    document.getElementById('psi_blast_filters').disabled=false;
 }
