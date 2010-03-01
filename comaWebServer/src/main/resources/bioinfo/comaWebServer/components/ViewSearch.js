b6bde147cf0290d2b6c14c0f478fce237512e4bf
Mindaugas.Laganeckas <Mindaugas.Laganeckas@549d8ce6-d049-11de-83be-cfe0592a616e>
2010-03-01 07:43:09 +0000

    Java script sensitively reacts to non existing form components.

--- comaWebServer/src/main/resources/bioinfo/comaWebServer/components/ViewSearch.js
+++ comaWebServer/src/main/resources/bioinfo/comaWebServer/components/ViewSearch.js
@@ -2,7 +2,7 @@ function disableGroup()
 {
     document.getElementById('seqDB').disabled=true;
     document.getElementById('lc_t').disabled=true;
-    document.getElementById('aa').disabled=true;
+    //document.getElementById('aa').disabled=true;
     document.getElementById('lc_h').disabled=true;
     document.getElementById('lc_j').disabled=true;
     document.getElementById('psi_blast_filters').disabled=true;
@@ -11,7 +11,7 @@ function disableGroup()
 function enableGroup()
 {
 	document.getElementById('seqDB').disabled=false;
-    document.getElementById('aa').disabled=false;
+    //document.getElementById('aa').disabled=false;
     document.getElementById('lc_h').disabled=false;
     document.getElementById('lc_j').disabled=false;
     document.getElementById('lc_t').disabled=false;
