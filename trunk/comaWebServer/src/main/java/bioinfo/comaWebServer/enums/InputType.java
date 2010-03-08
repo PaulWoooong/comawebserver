7af05723713849beb902e6394984e8b31f02a831
Mindaugas.Laganeckas <Mindaugas.Laganeckas@549d8ce6-d049-11de-83be-cfe0592a616e>
2010-03-08 10:26:19 +0000

    Gave a name for input types.

--- trunk/comaWebServer/src/main/java/bioinfo/comaWebServer/enums/InputType.java
+++ trunk/comaWebServer/src/main/java/bioinfo/comaWebServer/enums/InputType.java
@@ -2,5 +2,16 @@ package bioinfo.comaWebServer.enums;
 
 public enum InputType 
 {
-	FASTA, 
+	FASTA	("FASTA");
+
+	private final String name;
+	
+	InputType(String name)
+	{
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
 }
