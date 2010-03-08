bcead9e933865821880c5ce080ea5ecbaa1645cc
Mindaugas.Laganeckas <Mindaugas.Laganeckas@549d8ce6-d049-11de-83be-cfe0592a616e>
2010-03-08 13:18:19 +0000

    Wrote a GridDataSource for Job class.

---
+++ comaWebServer/src/main/java/bioinfo/comaWebServer/util/JobGridDataSource.java
@@ -0,0 +1,70 @@
+package bioinfo.comaWebServer.util;
+
+import java.util.List;
+
+import org.apache.tapestry5.grid.GridDataSource;
+import org.apache.tapestry5.grid.SortConstraint;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import bioinfo.comaWebServer.entities.Job;
+import bioinfo.comaWebServer.services.InitSessionFactory;
+
+public class JobGridDataSource implements GridDataSource 
+{
+	private static final String JOB_TABLE = " Job ";
+	private List<Job> selectedJobs = null;
+	private int from = 0;
+	
+	@Override
+	public int getAvailableRows() 
+	{
+		String queryStr = "select count(*) from " + JOB_TABLE;
+		
+		Session session = InitSessionFactory.getInstance().getCurrentSession();
+	    Transaction transaction = session.beginTransaction();
+
+		Query query = session.createQuery(queryStr);
+		List list = query.list();
+		
+		transaction.commit();
+
+		return (Integer)list.get(0);
+	}
+
+	@Override
+	public Class getRowType() 
+	{
+		return Job.class;
+	}
+
+	@Override
+	public Object getRowValue(int arg0) 
+	{
+		return selectedJobs.get(arg0);
+	}
+
+	@Override
+	public void prepare(int startIndex, int endIndex, List<SortConstraint> sortConstraints) 
+	{
+	    Transaction transaction = null;
+	    Session session = InitSessionFactory.getInstance().getCurrentSession();
+
+    	try
+    	{
+			transaction = session.beginTransaction();
+			Query query = session.createQuery("from " + JOB_TABLE + " o ");
+			selectedJobs = query.list();
+			from = startIndex;
+			transaction.commit();
+		}
+    	catch (HibernateException e)
+    	{
+    		if(transaction != null) transaction.rollback();
+    		throw e;
+		}
+	}
+
+}
